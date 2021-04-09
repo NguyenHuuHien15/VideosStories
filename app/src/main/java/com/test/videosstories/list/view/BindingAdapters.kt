7b8e41363a5152f8d7d5a7280ed0494e2e380a75
NGUYEN Huu Hien <hienka99@gmail.com>
2021-04-09 22:19:09 +0200

    Complete view and data of each item in the list

--- app/src/main/java/com/test/videosstories/list/view/BindingAdapters.kt
+++ app/src/main/java/com/test/videosstories/list/view/BindingAdapters.kt
@@ -1,12 +1,14 @@
 package com.test.videosstories.list.view
 
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.test.videosstories.R
 import com.test.videosstories.list.model.ItemForView
+import org.apache.commons.lang3.StringUtils
 
 @BindingAdapter("url")
 fun bindImage(imgView: ImageView, item: ItemForView?) {
@@ -19,4 +21,16 @@ fun bindImage(imgView: ImageView, item: ItemForView?) {
             RequestOptions().placeholder(R.drawable.loading_animation).error(R.drawable.ic_broken_image)
         ).into(imgView)
     }
+}
+
+@BindingAdapter("content")
+fun setContentOfTextView(view: TextView, item: ItemForView?) {
+    if (item == null) return
+
+    if (item.isVideo) {
+        view.text = item.views.toString() + StringUtils.SPACE + "views"
+    } else {
+        view.text = "By" + StringUtils.SPACE + item.author
+    }
+
 }
