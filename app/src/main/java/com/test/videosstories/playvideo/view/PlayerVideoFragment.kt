cbfb5c89f5bdd81448f5493cc977244c3f4e5de9
NGUYEN Huu Hien <hienka99@gmail.com>
2021-04-11 05:29:27 +0200

    Play video by ExoPlayer

--- app/src/main/java/com/test/videosstories/playvideo/view/PlayerVideoFragment.kt
+++ app/src/main/java/com/test/videosstories/playvideo/view/PlayerVideoFragment.kt
@@ -1,15 +1,17 @@
 package com.test.videosstories.playvideo.view
 
+import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
 import android.view.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.util.Util
 import com.test.videosstories.databinding.FragmentPlayerVideoBinding
-import com.test.videosstories.databinding.FragmentStoryDetailsBinding
-import com.test.videosstories.detail.viewmodel.StoryDetailsViewModel
-import com.test.videosstories.detail.viewmodel.StoryDetailsViewModelFactory
 import com.test.videosstories.playvideo.viewmodel.PlayerVideoViewModel
 import com.test.videosstories.playvideo.viewmodel.PlayerVideoViewModelFactory
 
@@ -19,6 +21,12 @@ class PlayerVideoFragment : Fragment() {
     private lateinit var dataBinding: FragmentPlayerVideoBinding
     private lateinit var viewModel: PlayerVideoViewModel
     private lateinit var viewModelFactory: PlayerVideoViewModelFactory
+
+    private lateinit var playerView: PlayerView
+    private var player: SimpleExoPlayer? = null
+    private var playWhenReady = true
+    private var currentWindow = 0
+    private var playbackPosition: Long = 0
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         dataBinding = FragmentPlayerVideoBinding.inflate(layoutInflater, container, false)
@@ -49,6 +57,68 @@ class PlayerVideoFragment : Fragment() {
                 viewModel.doneBackToHome()
             }
         })
+
+        playerView = dataBinding.videoView
     }
 
+    override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT > 23) {
+            initializePlayer()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        hideSystemUi()
+        if (Util.SDK_INT <= 23 || player == null) {
+            initializePlayer()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (Util.SDK_INT <= 23) {
+            releasePlayer()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (Util.SDK_INT > 23) {
+            releasePlayer()
+        }
+    }
+
+    private fun initializePlayer() {
+        player = SimpleExoPlayer.Builder(requireContext()).build()
+        playerView.player = player
+        val mediaItem = viewModel.urlVideo.value?.let { MediaItem.fromUri(it) }
+        if (mediaItem != null) {
+            player!!.setMediaItem(mediaItem)
+        }
+        player!!.playWhenReady = playWhenReady
+        player!!.seekTo(currentWindow, playbackPosition)
+        player!!.prepare()
+    }
+
+    private fun releasePlayer() {
+        if (player != null) {
+            playbackPosition = player!!.currentPosition
+            currentWindow = player!!.currentWindowIndex
+            playWhenReady = player!!.playWhenReady
+            player!!.release()
+            player = null
+        }
+    }
+
+    @SuppressLint("InlinedApi")
+    private fun hideSystemUi() {
+        /*playerView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LOW_PROFILE
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)*/
+    }
 }
