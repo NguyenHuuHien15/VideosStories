78af690514914ce66c633eb53abdbed6c1b3c51e
NGUYEN Huu Hien <hienka99@gmail.com>
2021-04-11 19:52:03 +0200

    DI: Use Dagger 2

--- app/src/main/java/com/test/videosstories/common/MyApplication.kt
+++ app/src/main/java/com/test/videosstories/common/MyApplication.kt
@@ -1,6 +1,15 @@
 package com.test.videosstories.common
 
 import android.app.Application
+import com.test.videosstories.common.di.AppComponent
+import com.test.videosstories.common.di.DaggerAppComponent
 
 open class MyApplication : Application() {
+    val appComponent: AppComponent by lazy {
+        initializeComponent()
+    }
+
+    open fun initializeComponent(): AppComponent {
+        return DaggerAppComponent.factory().create(applicationContext)
+    }
 }
